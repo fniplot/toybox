02d0ffed030115ebc542b5e8ac04fae73f0198f6
Keisuke Fuse <cyari.buran@gmail.com>
2021-11-06 00:45:47 +0900

    必要最小限の実装

---
+++ app/src/main.js
@@ -0,0 +1,18 @@
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount('#app')
